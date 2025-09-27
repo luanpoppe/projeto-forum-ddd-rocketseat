48191f4dc08b6f2ccd8d5e7bfa3d6b9881a13f58
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-26 22:54:55 -0300

    feat: adicionando getters das classes de entidade

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
 import { UniqueentityID } from "../../core/entities/unique-entity-id";
@@ -8,19 +9,79 @@ interface QuestionProps {
   content: string;
   slug: Slug;
   authorId: UniqueentityID;
-  bestAnswerId?: UniqueentityID;
+  bestAnswerId?: UniqueentityID | undefined;
   createdAt: Date;
   updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get isNew() {
+    return dayjs().diff(this.createdAt, "days") <= 3;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
+  }
+
+  set content(content) {
+    this.props.content = content;
+
+    this.touch();
+  }
+
+  set bestAnswerId(bestAnswerId) {
+    this.props.bestAnswerId = bestAnswerId;
+
+    this.touch();
+  }
+
+  set title(title) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+
+    this.touch();
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
   static create(
-    props: Optional<QuestionProps, "createdAt">,
+    props: Optional<QuestionProps, "createdAt" | "slug">,
     id?: UniqueentityID
   ) {
     const question = new Question(
       {
         ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: new Date(),
       },
       id
